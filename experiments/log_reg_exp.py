b32a92924b0ba5640512fea77141b7e4f0c06058
Maja Rapp <maja.rapp@gmx.de>
2025-11-29 21:10:20 +0100

    Train and evaluate logistic regression model

---
+++ experiments/log_reg_exp.py
@@ -0,0 +1,49 @@
+# At the beginning of your Python script
+from dotenv import load_dotenv
+import mlflow
+import numpy as np
+import reusableFunctions
+
+from sklearn.linear_model import LogisticRegression
+
+# Load environment variables from .env file
+load_dotenv(".databricks/.databricks.env")
+
+exp_name = "/Users/majathe.rapp@gmail.com/test"
+
+params = {
+    "solver": "lbfgs",
+    "max_iter": 10,
+    "random_state": reusableFunctions.rand_state,
+}
+
+
+# # Test logging to verify connection
+# print(f"MLflow Tracking URI: {mlflow.get_tracking_uri()}")
+# with mlflow.start_run():
+#     print("✓ Successfully connected to MLflow!")
+
+
+def connectToExperiment(name):
+    try:
+        print(f"MLflow Tracking URI: {mlflow.get_tracking_uri()}")
+
+        # Set the experiment path in the remote server
+        mlflow.set_experiment(name)
+    except Exception as e:
+        print(e)
+    
+
+
+if __name__ == "__main__":
+    print(f"Start Experiment in {exp_name}")
+    #connectToExperiment()
+
+    X_train,  X_test,  y_train,  y_test = reusableFunctions.loadTrainTestData()
+
+    model = LogisticRegression(max_iter=params["max_iter"], random_state=params["random_state"], solver=params["solver"])
+    model.fit(X_train, y_train)
+
+    pred = model.predict(X_test)
+
+    reusableFunctions.evaluateModel(y_test, pred)
--- experiments/reusableFunctions.py
+++ experiments/reusableFunctions.py
@@ -1,17 +1,33 @@
 import numpy as np
 import pandas as pd
 
+from box import ConfigBox
+from ruamel.yaml import YAML
+import numpy as np
+
+yaml = YAML(typ="safe")
+
+params = ConfigBox(yaml.load(open("params.yaml", encoding="utf-8")))
+y_column = params.data.y_column
+rand_state = params.base.random_seed
+
+from sklearn.metrics import accuracy_score, classification_report
+
 def loadTrainTestData():
     try:
         X_train = np.load('./data/train_data/X_train.npy')
         X_test = np.load('./data/test_data/X_test.npy')
-        y_train = pd.read_csv('./data/train_data/y_train.csv')
-        y_test = pd.read_csv('./data/test_data/y_test.csv')
+        y_train = pd.read_csv('./data/train_data/y_train.csv')[y_column]
+        y_test = pd.read_csv('./data/test_data/y_test.csv')[y_column]
 
         return  X_train,  X_test,  y_train,  y_test
     except Exception as e:
         print(e)
-    
+
+def evaluateModel(y_test, pred):
+    print("Accuracy:", accuracy_score(y_test, pred))
+    print(classification_report(y_test, pred))
+
 
 if __name__ == "__main__":
     print(f"Loading training and test data")
@@ -20,5 +36,7 @@ if __name__ == "__main__":
 
     #print(X_train[0:5])
     #print(y_test.head())
+    #print(X_train[0:5])
+    #print(y_test.shape)
 
     print ("Data successfully loaded")

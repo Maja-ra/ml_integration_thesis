3ff0185a07223919179735efcfe95d5e8a15c6b8
Maja Rapp <maja.rapp@gmx.de>
2025-12-01 02:50:03 +0100

    MLFlow tracking dagshub for log reg model experiment

--- experiments/log_reg_exp.py
+++ experiments/log_reg_exp.py
@@ -1,19 +1,26 @@
 # At the beginning of your Python script
-from dotenv import load_dotenv
+
 import mlflow
 import numpy as np
 import reusableFunctions
+from mlflow.models import infer_signature
+import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 
-# Load environment variables from .env file
-load_dotenv(".databricks/.databricks.env")
 
-exp_name = "/Users/majathe.rapp@gmail.com/thesis_log_reg"
+MLFLOW_HOST_SERVER, exp_workspace_url = reusableFunctions.initializeMlflowServer()
+
+exp_name = "log_reg"
+model_name = "log_reg_treatment_model"
+
+if MLFLOW_HOST_SERVER == "databricks":
+    exp_name = exp_workspace_url + exp_name
 
 params = {
     "solver": "lbfgs",
-    "max_iter": 10,
+    "max_iter": 100,
     "random_state": reusableFunctions.rand_state,
 }
 
@@ -39,17 +46,86 @@ if __name__ == "__main__":
     print(f"Start Experiment in {exp_name}")
     connectToExperiment(exp_name)
 
-    # Enable autologging for scikit-learn
-    mlflow.sklearn.autolog()
+    dvc_url_clean = reusableFunctions.getDvcDatasetVersion("clean_data", "clean_data.csv")
+    dvc_url_train = reusableFunctions.getDvcDatasetVersion("train_data", "X_train.npy")
+    dvc_url_test = reusableFunctions.getDvcDatasetVersion("test_data", "X_test.npy")
 
+    dvc_params = {'dvc_clean_data_url': dvc_url_clean, 'dvc_train_data_url': dvc_url_train, 'dvc_test_data_url': dvc_url_test}
+
+
+    # Log dataset version in MLflow
+    # with mlflow.start_run():
+    #     mlflow.log_artifact("data/clean_data", artifact_path="datasets")
+
+    # Enable autologging for scikit-learn
+    # mlflow.sklearn.autolog()
+
+    clean_data = reusableFunctions.loadCleanData()
     X_train,  X_test,  y_train,  y_test = reusableFunctions.loadTrainTestData()
 
-    try:
-        model = LogisticRegression(max_iter=params["max_iter"], random_state=params["random_state"], solver=params["solver"])
-        model.fit(X_train, y_train)
+    dataset, dataset_train, dataset_test = reusableFunctions.mlflowSetDatasets(clean_data, X_train,  X_test,  y_train,  y_test)
+
+    model = LogisticRegression(max_iter=params["max_iter"], random_state=params["random_state"], solver=params["solver"])
+
+    try: 
+        with mlflow.start_run():
+            model.fit(X_train, y_train)
+            mlflow.log_params(dvc_params)
+            mlflow.log_params(params)
+            mlflow.log_input(dataset, context="clean data")
+            mlflow.log_input(dataset_train, context="training")
+            mlflow.log_input(dataset_test, context="testing")
+
+            pred = model.predict(X_test)
+            reusableFunctions.evaluateModel(y_test, pred) # prints evaluation report
+
+            signature = infer_signature(X_train[:1], y_train[:1])      # creates input example to validate and insure consistent input, X_test, model.predict(X_test)
+            print(signature)
+            #model_info = 
+            #mlflow.sklearn.log_model(model, artifact_path = "log_reg_model", signature=signature, registered_model_name=model_name) #artifact_path=model_name
+            # mlflow.sklearn.log_model(
+            # model, 
+            # artifact_path= model_name, 
+            # signature=signature
+            # )
+
+
+            # Build the Evaluation Dataset from the test set
+            #eval_data = pd.DataFrame(X_test)
+            #print(eval_data.head())
+            #print(eval_data.shape)
+            #eval_data["label"] = y_test
+            #eval_data["predictions"] = pred
+
+            
+
+            eval_params = classification_report(y_test, pred, output_dict=True)
+
+            # Evaluate the static dataset without providing a model -> timeout
+            # result = mlflow.evaluate(
+            #     data=eval_data,
+            #     targets="label",
+            #     predictions="predictions",
+            #     model_type="classifier",
+            # )
+
+            print(eval_params)
+            mlflow.log_metrics(eval_params["weighted avg"])
+            mlflow.log_metric("accuracy", eval_params["accuracy"])
+            #mlflow.log_metrics(result.metrics)
+
+        # model.fit(X_train, y_train)
+        # pred = model.predict(X_test)
+        # reusableFunctions.evaluateModel(y_test, pred)
+        
     except Exception as e:
         print(e)
 
-    pred = model.predict(X_test)
+    # try:
 
-    reusableFunctions.evaluateModel(y_test, pred)
+    # except Exception as e:
+    #     print(e)
+
+
+
+    
--- experiments/reusableFunctions.py
+++ experiments/reusableFunctions.py
@@ -4,6 +4,13 @@ import pandas as pd
 from box import ConfigBox
 from ruamel.yaml import YAML
 import numpy as np
+import dvc.api
+import mlflow
+from pathlib import Path
+import dagshub
+from dotenv import load_dotenv
+import os
+
 
 yaml = YAML(typ="safe")
 
@@ -12,6 +19,44 @@ y_column = params.data.y_column
 rand_state = params.base.random_seed
 
 from sklearn.metrics import accuracy_score, classification_report
+
+
+def initializeMlflowServer():
+    try:
+        load_dotenv("environments/dev.env")
+
+        MLFLOW_HOST_SERVER = os.getenv("MLFLOW_HOST_SERVER")
+    except Exception as e:
+        print(e)
+
+    # print(MLFLOW_HOST_SERVER)
+
+    try:
+
+        if MLFLOW_HOST_SERVER == "databricks":
+
+            # Load environment variables from .env file
+            load_dotenv(".databricks/.databricks.env")
+
+            #exp_name = "/Users/majathe.rapp@gmail.com/thesis_log_reg"
+            exp_workspace_url = "/Users/majathe.rapp@gmail.com/"
+
+            print("Mlflow server set to Databricks")
+
+
+        elif MLFLOW_HOST_SERVER == "dagshub":
+
+            exp_workspace_url = 'ml_integration_thesis'
+
+            dagshub.init(repo_owner='Maja-ra', repo_name=exp_workspace_url, mlflow=True)
+            print("Mlflow server set to Dagshub")
+
+    except Exception as e:
+        print(e)
+
+    return MLFLOW_HOST_SERVER, exp_workspace_url
+
+
 
 def loadTrainTestData():
     try:
@@ -24,15 +69,98 @@ def loadTrainTestData():
     except Exception as e:
         print(e)
 
+def loadCleanData():
+    try:
+        clean_data = pd.read_csv('./data/clean_data/clean_data.csv', index_col=None)
+
+        return  clean_data
+    except Exception as e:
+        print(e)
+
+#subfolder of datadirectory eg. "train_data"
+def getDvcDatasetVersion(subfolder_pathstring, dataset_name):
+    # Fetch dataset version from DVC
+    if subfolder_pathstring == "":
+        data_path_string = "data/" + dataset_name
+    else:
+        data_path_string = "data/" + subfolder_pathstring + "/" + dataset_name
+
+    repos_path = Path().absolute()
+    repos_path = str(repos_path).replace("\\", "/")
+
+    try:
+        dvc_url = dvc.api.get_url(path = data_path_string, repo = repos_path)
+        print(f" DVC data url for {dataset_name} is: {dvc_url}")
+
+        return dvc_url
+    except Exception as e:
+        print(e)
+
+    
+
+    #print(data_path_string)
+    #print(repos_path)
+    
+    
+def mlflowSetDatasets(clean_data_df, X_train, X_test, y_train, y_test):
+    cols = list(clean_data_df.columns)
+    # print(clean_data_df.head())
+    # print(type(cols))
+
+    cols.remove("treatment")
+
+    # print(cols)
+    # print(len(cols))
+    # print(X_train.shape)
+    # print(X_train[0:5])
+    # print(y_train.value_counts())
+    # print(y_train.shape)
+    
+    train_data_df = pd.DataFrame(X_train, columns=cols)
+    train_data_df["treatment"] = y_train
+
+    test_data_df = pd.DataFrame(X_test, columns=cols)
+    test_data_df["treatment"] = y_test
+
+    # print(train_data_df.shape)
+    # print(train_data_df.head())
+
+    # print(train_data_df.head())
+
+    dataset = mlflow.data.from_pandas(clean_data_df,
+                                      source="data/clean_data/clean_data.csv", 
+                                      name="Mental Health Dataset local - Cleaned", 
+                                      targets=y_column
+                                      )
+    
+    dataset_train = mlflow.data.from_pandas(train_data_df, 
+                                            source = "data/train_data/X_train.npy",
+                                            name="Training dataset", 
+                                            targets=y_column
+                                            )
+
+    dataset_test = mlflow.data.from_pandas(test_data_df, 
+                                            source = "data/test_data/X_test.npy",
+                                            name="Testing dataset", 
+                                            targets=y_column
+                                            )    
+    
+    
+    return dataset, dataset_train, dataset_test
+    # # Log dataset version in MLflow
+    # mlflow.log_artifact(dataset_path, artifact_path="datasets")
+
 def evaluateModel(y_test, pred):
     print("Accuracy:", accuracy_score(y_test, pred))
     print(classification_report(y_test, pred))
 
 
+
+
 if __name__ == "__main__":
     print(f"Loading training and test data")
 
-    X_train,  X_test,  y_train,  y_test = loadTrainTestData()
+    #X_train,  X_test,  y_train,  y_test = loadTrainTestData()
 
     #print(X_train[0:5])
     #print(y_test.head())
@@ -40,3 +168,20 @@ if __name__ == "__main__":
     #print(y_test.shape)
 
     print ("Data successfully loaded")
+
+    #print(getDvcDatsetVersion("train_data", "X_train.npy"))
+
+    #clean_data = loadCleanData()
+    #X_train,  X_test,  y_train,  y_test = loadTrainTestData()
+
+    #dataset, dataset_train = 
+    #dataset, dataset_train, dataset_test = mlflowSetDatasets(clean_data, X_train, y_train, X_test, y_test)
+
+    print("datasets defined")
+
+
+    server, exp_workspace_url = initializeMlflowServer()
+
+    #print(server)
+    #print(exp_workspace_url)
+
